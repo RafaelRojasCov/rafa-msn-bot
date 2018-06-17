49dd6fbe505448b8b47e41e11b379a138aa8cc09
Rafael Rojas <rafaelrojas.cov@gmail.com>
2018-06-17 01:00:12 -0400

    adding env var

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -36,7 +38,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  let VERIFY_TOKEN = PAGE_ACCESS_TOKEN
     
   // Parse the query params
   let mode = req.query['hub.mode'];
